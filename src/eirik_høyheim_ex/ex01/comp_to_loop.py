217c023aa76caa89e05983bf75f647eeaa03068e
Eirik Høyheim <eirihoyh@nmbu.no>
2019-09-19 12:36:50 +0200

    Made the function squares_by_loop to do the same as squares_by_comp using a for-loop

--- src/eirik_høyheim_ex/ex01/comp_to_loop.py
+++ src/eirik_høyheim_ex/ex01/comp_to_loop.py
@@ -1,13 +1,15 @@
 def squares_by_comp(n):
-    return [k**2 for k in range(n) if k % 3 == 1]
+    return [k ** 2 for k in range(n) if k % 3 == 1]
+
 
 def squares_by_loop(n):
-    '''made a for-loop'''
+    """makes a for-loop and does the same as squares_by_comp function"""
     list_ = []
     for k in range(n):
         if k % 3 == 1:
-            list_.append(k**2)
+            list_.append(k ** 2)
     return list_
+
 
 if __name__ == '__main__':
     if squares_by_comp(2) != squares_by_loop(2):
