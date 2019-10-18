2b7dd08ba548babb740b8753a92d9f0b21c5c265
Eirik Høyheim <eirihoyh@nmbu.no>
2019-10-18 15:55:08 +0200

    Made the function and created the two classes. Did not fill it with anything

---
+++ src/YOUR_NAME_ex/ex04/myrand.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Eirik Høyheim'
+__email__ = 'eirihoyh@nmbu.no'
+
+import random
+
+
+class LCGRand:
+    def __init__(self):
+        pass
+
+    def rand(self):
+        pass
+
+
+class ListRand:
+    def __init__(self):
+        pass
