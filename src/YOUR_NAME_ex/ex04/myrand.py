2ce61531d70cdba6529f2acff11dc649cd990854
Eirik Høyheim <eirihoyh@nmbu.no>
2019-10-21 13:11:12 +0200

    Filled in some of the self's in LCGRand. In ListRand I filled in the self's and begun on the rand function. rand does not work fully yet

--- src/YOUR_NAME_ex/ex04/myrand.py
+++ src/YOUR_NAME_ex/ex04/myrand.py
@@ -7,13 +7,25 @@ import random
 
 
 class LCGRand:
-    def __init__(self):
-        pass
+    def __init__(self, seed):
+        self.a = 7**5
+        self.m = (2**31)-1
+        self.n = 0
+        self.seed = seed
 
     def rand(self):
         pass
 
 
 class ListRand:
-    def __init__(self):
-        pass
+    def __init__(self, numberlist):
+        self.numberlist = numberlist
+        self.position = 0
+
+    def rand(self):
+        try:
+            k = self.numberlist[self.position]
+            self.position += 1
+            return k
+        except RuntimeError:
+            print('The list is empty')
