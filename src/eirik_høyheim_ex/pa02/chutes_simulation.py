03dc25807a853ceb43731360bad7052e4977c2a4
mesenbet A.Delele <mesenbea@nmbu.no>
2019-11-23 23:59:45 +0100

    Attepted to write a test for most of the functions

--- src/eirik_høyheim_ex/pa02/chutes_simulation.py
+++ src/eirik_høyheim_ex/pa02/chutes_simulation.py
@@ -69,7 +69,10 @@ class ResilientPlayer(Player):
             self.position += self.extra_steps
 
         self.n_steps += 1
+
+
 class LazyPlayer(Player):
+
     default_dropped_steps = 1
 
     def __init__(self, board, dropped_steps=None):
@@ -94,11 +97,11 @@ class LazyPlayer(Player):
 
 class Simulation:
 
-    def __init__(self, player_field, board=None, seed=45,
+    def __init__(self, player_field, board=None, seed=450,
                  randomize_players=False):
         self.randomize_players = randomize_players
         self.player_list = player_field
-<<<<<<< HEAD
+
         # if board is None:
         #     self.board = Board()
         # else:
@@ -108,23 +111,12 @@ class Simulation:
         # #     pass
         self.board = board if board is not None else Board()
 
-        if self.randomize_players is True:
-            self.player_list = random.shuffle(self.player_list)
-        self.random = self.randomize_players
-        self.multi_sim = None  # Stores all the results from run_simulation
-        self.the_best_results = []
-=======
-        if board is None:
-            self.board = Board()
-        else:
-            self.board = board
         if randomize_players is True:
             self.player_list = random.shuffle(self.player_list)
 
         self.multi_sim = None
         self.the_best_results = []
 
->>>>>>> pa02
         random.seed(seed)
 
         self.P_type = [player.__name__ for player in self.player_list or []]
@@ -134,14 +126,11 @@ class Simulation:
         takes in nothing, except whats in the __init__  function.
         Returns the winner and how may steps the winner took
         """
-<<<<<<< HEAD
 
-        players = [player(self.board) for player in self.player_list]
         # if self.random:
         #     random.shuffle(players)
-=======
+
         players = [player(self.board) for player in self.player_list]
->>>>>>> pa02
 
         while True:
             for player in players:
@@ -155,19 +144,11 @@ class Simulation:
         single_game, but does not return anything, maybe make some more self
         in __init__ ?
         """
-<<<<<<< HEAD
 
-        for _ in range(n_sims):
-            self.the_best_results.append(self.single_game())
-=======
         for _ in range(n_sims):  # runs n_sims times...
             self.the_best_results.append(
                 self.single_game())  # runs the single_game
             # and puts the result into the_best_results list
-
-        self.multi_sim = self.the_best_results  # sets multi_sim =
-        # the_best_result so the Simulation class remembers the results
->>>>>>> pa02
 
     def get_results(self):
         """
@@ -196,23 +177,10 @@ class Simulation:
         {'Player': [11, 25, 13], 'LazyPlayer': [39],
         'ResilientPlayer': [8, 7, 6, 11}
         """
-<<<<<<< HEAD
 
         return {player_type: [d for d, t in self.the_best_results
                               if t == player_type]
                 for player_type in self.P_type}
-=======
-        duration_dict = {'Player': [], 'LazyPlayer': [], 'ResilientPlayer': []}
-        for score, player in self.multi_sim:
-            if player == 'Player':
-                duration_dict['Player'].append(score)
-            if player == 'LazyPlayer':
-                duration_dict['LazyPlayer'].append(score)
-            if player == 'ResilientPlayer':
-                duration_dict['ResilientPlayer'].append(score)
-
-        return duration_dict
->>>>>>> pa02
 
     def players_per_type(self):
         """
@@ -221,20 +189,14 @@ class Simulation:
         {'Player': 3, 'LazyPlayer': 1, 'ResilientPlayer': 0}
         """
 
-<<<<<<< HEAD
-        return {player_type.__name__:
-                    self.player_list.count(player_type)
+        return {player_type.__name__: self.player_list.count(player_type)
                 for player_type in self.player_list}
-=======
-        return {player_type.__name__: self.player_list.count(player_type) for
-                player_type in self.player_list}
->>>>>>> pa02
 
 
 if __name__ == '__main__':
     sim = Simulation([Player, LazyPlayer, ResilientPlayer])
     print(sim.single_game())
-    sim.run_simulation(10)
+    sim.run_simulation(7)
     print(sim.get_results())
     print(sim.winners_per_type())
     print(sim.durations_per_type())
--- src/eirik_høyheim_ex/pa02/test_pa02.py
+++ src/eirik_høyheim_ex/pa02/test_pa02.py
@@ -6,6 +6,58 @@ __email__ = "eirihoyh@nmbu.no, mesenbea@nmbu.no"
 import chutes_simulation as cs
 
 import pytest
+import unittest
+
+
+class TestBoard:
+    """
+
+    Tests for Board class.
+
+    """
+    def test_constructor_default(self):
+        """Default constructor callable."""
+        b = cs.Board()
+        assert isinstance(b, cs.Board)
+
+    def test_constructor_args(self):
+        """Constructor with unnamed arguments callable."""
+        b = cs.Board([(1, 4), (5, 16)], [(9, 2), (12, 3)], 90)
+        assert isinstance(b, cs.Board)
+
+    def test_constructor_named_args(self):
+        """Constructor with kw args callable."""
+        b = cs.Board(ladders=[(1, 4), (5, 16)], chutes=[(9, 2), (12, 3)], goal=90)
+        assert isinstance(b, cs.Board)
+
+        # The following test asserts if the user gives the right value for
+        # the ladders start and end values
+        start_end_ladder = [[st, end] for st, end in self.ladders]
+        assert start_end_ladder[0] < start_end_ladder[1]
+
+        # The following test asserts if the user gives the right value for
+        #   the chutes start and end values
+        start_end_chutes = [[st, end] for st, end in self.chutes]
+        assert start_end_chutes[0] > start_end_chutes[1]
+
+        # this asserts if the goal is > 0
+        assert self.goal > 0
+        if not isinstance(b, cs.Board):
+            raise TypeError('Please provide the right value for '
+                            'chutes, ladders or goal')
+
+    def test_goal_reached(self, position):
+        """goal_reached() callable and returns bool"""
+        b = cs.Board()
+        assert isinstance(b.goal_reached(1), bool)
+        if b.goal_reached(1):
+            result = b.goal_reached(1)
+            assert result == True
+
+    def test_position_adjustment(self):
+        """position_adjustment callable and returns number"""
+        b = cs.Board()
+        assert isinstance(b.position_adjustment(1), (int, float))
 
 
 class TestPlayer:
@@ -85,3 +137,107 @@ class TestSimulation:
         s.run_simulation(3)
         r = s.get_results()
         assert isinstance(r, list)
+
+    def test_single_game(self):
+        """single_game() returns non-negative number and class name"""
+
+        s = cs.Simulation([cs.Player, cs.Player])
+
+        nos, wc = s.single_game()
+
+        assert nos > 0
+
+        assert wc == 'Player'
+        s450 = cs.Simulation(7, board=None, seed=450, randomize_players=False)
+        assert s450.single_game() == (11, 'Player')
+
+    def test_simulation_results(self):
+        """
+        - Multiple calls to run_simulation() aggregate results
+        - get_results() returns list of result tuples
+        """
+        s = cs.Simulation([cs.Player, cs.Player])
+        s.run_simulation(2)
+        r = s.get_results()
+        assert len(r) == 2
+        s.run_simulation(1)
+        r = s.get_results()
+        assert len(r) == 3
+        assert all(s > 0 and t == 'Player' for s, t in r)
+
+        player_list = 7
+        s450 = cs.Simulation(player_list, board=None, seed=450,
+                             randomize_players=False)
+
+        assert s450.get_results() == [
+            (16, 'Player'), (16, 'Player'), (34, 'LazyPlayer'),
+            (24, 'ResilientPlayer'), (18, 'ResilientPlayer'),
+            (8, 'ResilientPlayer'), (30, 'LazyPlayer')
+        ]
+
+    def test_players_per_type(self):
+        """player_per_type() returns dict mapping names to non-neg numbers."""
+
+        s = cs.Simulation([cs.Player, cs.LazyPlayer, cs.ResilientPlayer])
+
+        p = s.players_per_type()
+
+        assert all(k in ['Player', 'LazyPlayer', 'ResilientPlayer']
+
+                   for k in p.keys())
+
+        assert all(v >= 0 for v in p.values())
+
+        player_list = 7
+        s450 = cs.Simulation(player_list, board=None, seed=450,
+                             randomize_players=False)
+        assert s450.players_per_type() == {
+            'Player': 1, 'LazyPlayer': 1, 'ResilientPlayer': 1
+        }
+
+    def test_winners_per_type(self):
+        """winners_per_type() returns dict mapping names to non-neg numbers."""
+        s = cs.Simulation([cs.Player, cs.LazyPlayer, cs.ResilientPlayer])
+        s.run_simulation(10)
+        w = s.winners_per_type()
+        assert all(k in ['Player', 'LazyPlayer', 'ResilientPlayer']
+                   for k in w.keys())
+        assert all(v >= 0 for v in w.values())
+
+        player_list = 7
+        s450 = cs.Simulation(player_list, board=None, seed=450,
+                             randomize_players=False)
+        assert s450.winners_per_type() == {
+            'Player': 2, 'LazyPlayer': 2, 'ResilientPlayer': 3
+        }
+
+    def test_durations_per_type(self):
+        """
+
+        durations_per_type() returns dict mapping names to list of
+
+        non-neg numbers.
+
+        """
+
+        s = cs.Simulation([cs.Player, cs.LazyPlayer, cs.ResilientPlayer])
+
+        s.run_simulation(10)
+
+        w = s.durations_per_type()
+
+        assert all(k in ['Player', 'LazyPlayer', 'ResilientPlayer']
+
+                   for k in w.keys())
+
+        assert all(len(v) >= 0 for v in w.values())
+
+        assert all(n >= 0 for v in w.values() for n in v)
+        player_list = 7
+        s450 = cs.Simulation(player_list, board=None, seed=450,
+                             randomize_players=False)
+
+        assert s450.durations_per_type() == {
+            'Player': [16, 16], 'LazyPlayer': [34, 30],
+            'ResilientPlayer': [24, 18, 8]
+        }
